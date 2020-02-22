a7302d5aefeddc97220bae7f8dd70a7df22e0637
Paulo Villela <pvillela@gmail.com>
2020-02-21 19:12:31 -0500

    Substantially enhanced coroutineExceptions.kt.

--- src/main/kotlin/tryout/coroutineExceptions.kt
+++ src/main/kotlin/tryout/coroutineExceptions.kt
@@ -1,14 +1,13 @@
 package tryout
 
 import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import reactor.core.scheduler.Schedulers
+import kotlinx.coroutines.supervisorScope
 
 
 // Based on
@@ -21,7 +20,7 @@ suspend fun f0() = coroutineScope {
     }
     job.join()
     println("Joined failed job")
-    val deferred = GlobalScope.async {
+    val deferred: Deferred<Int> = GlobalScope.async {
         println("Throwing exception from async")
         throw ArithmeticException("From async") // Nothing is printed, relying on user to call await
     }
@@ -33,7 +32,7 @@ suspend fun f0() = coroutineScope {
     }
 }
 
-suspend fun f1(waitTime: Long) = coroutineScope {
+suspend fun f1Launch(waitTime: Long) = coroutineScope {
     val job = launch {
         println("Throwing exception from launch")
         throw IndexOutOfBoundsException("From job")
@@ -49,13 +48,15 @@ suspend fun f1(waitTime: Long) = coroutineScope {
     println("... finished waiting $waitTime milliseconds.")
 
     try {
+        println("Joining failed job")
         job.join()
     } catch (e: Exception) {
-        println("Joined job")
+        println("Joined failed job")
         println("Caught inside: $e")
     }
 
     try {
+        println("Awaiting on deferred")
         deferred.await()
     } catch (e: Exception) {
         println("Awaited on deferred")
@@ -63,6 +64,380 @@ suspend fun f1(waitTime: Long) = coroutineScope {
     }
 
     job to deferred
+}
+
+suspend fun f1LaunchNoJoin(waitTime: Long) = coroutineScope {
+    val job = launch {
+        println("Throwing exception from launch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    job to deferred
+}
+
+suspend fun f1LaunchJoinAtEnd(waitTime: Long) = coroutineScope {
+    val job = launch {
+        println("Throwing exception from launch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Joining job")
+        job.join()
+    } catch (e: Exception) {
+        println("Exception thrown by job.join(): $e")
+        throw e
+    }
+
+    job to deferred
+}
+
+suspend fun f1Async(waitTime: Long) = coroutineScope {
+    val pseudoJob: Deferred<Int> = async {
+        println("Throwing exception from pseudoLaunch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Joining failed pseudoJob")
+        pseudoJob.join()
+    } catch (e: Exception) {
+        println("Joined failed pseudoJob")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Awaiting on failed pseudoJob")
+        pseudoJob.await()
+    } catch (e: Exception) {
+        println("Awaited on failed pseudoJob")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    pseudoJob to deferred
+}
+
+suspend fun f1AsyncNoJoin(waitTime: Long) = coroutineScope {
+    val pseudoJob: Deferred<Int> = async {
+        println("Throwing exception from pseudoLaunch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    pseudoJob to deferred
+}
+
+suspend fun f1AsyncJoinAtEnd(waitTime: Long) = coroutineScope {
+    val pseudoJob: Deferred<Int> = async {
+        println("Throwing exception from pseudoLaunch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Joining pseudoJob")
+        pseudoJob.join()
+    } catch (e: Exception) {
+        println("Exception thrown by pseudoJob.join(): $e")
+        throw e
+    }
+
+    try {
+        println("Awaiting on pseudoJob")
+        pseudoJob.await()
+    } catch (e: Exception) {
+        println("Exception thrown by pseudoJob.await(): $e")
+        throw e
+    }
+
+    pseudoJob to deferred
+}
+
+suspend fun f1LaunchS(waitTime: Long) = supervisorScope {
+    val job = launch {
+        println("Throwing exception from launch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Joining failed job")
+        job.join()
+    } catch (e: Exception) {
+        println("Joined failed job")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    job to deferred
+}
+
+suspend fun f1LaunchNoJoinS(waitTime: Long) = supervisorScope {
+    val job = launch {
+        println("Throwing exception from launch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    job to deferred
+}
+
+suspend fun f1LaunchJoinAtEndS(waitTime: Long) = supervisorScope {
+    val job = launch {
+        println("Throwing exception from launch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Joining job")
+        job.join()
+    } catch (e: Exception) {
+        println("Exception thrown by job.join(): $e")
+        throw e
+    }
+
+    job to deferred
+}
+
+suspend fun f1AsyncS(waitTime: Long) = supervisorScope {
+    val pseudoJob: Deferred<Int> = async {
+        println("Throwing exception from pseudoLaunch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Joining pseudoJob")
+        pseudoJob.join()
+    } catch (e: Exception) {
+        println("Joined failed pseudoJob")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Awaiting on pseudoJob")
+        pseudoJob.await()
+    } catch (e: Exception) {
+        println("Awaited on failed pseudoJob")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    pseudoJob to deferred
+}
+
+suspend fun f1AsyncNoJoinS(waitTime: Long) = supervisorScope {
+    val pseudoJob: Deferred<Int> = async {
+        println("Throwing exception from launch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    pseudoJob to deferred
+}
+
+suspend fun f1AsyncJoinAtEndS(waitTime: Long) = supervisorScope {
+    val pseudoJob: Deferred<Int> = async {
+        println("Throwing exception from launch")
+        throw IndexOutOfBoundsException("From job")
+    }
+
+    val deferred: Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException(("From async"))
+    }
+
+    println("Waiting ...")
+    delay(waitTime)
+    println("... finished waiting $waitTime milliseconds.")
+
+    try {
+        println("Awaiting on deferred")
+        deferred.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    try {
+        println("Joining pseudoJob")
+        pseudoJob.join()
+    } catch (e: Exception) {
+        println("Exception thrown by pseudoJob.join(): $e")
+        throw e
+    }
+
+    try {
+        println("Awaiting on pseudoJob")
+        pseudoJob.await()
+    } catch (e: Exception) {
+        println("Exception thrown by pseudoJob.await(): $e")
+        throw e
+    }
+
+    pseudoJob to deferred
 }
 
 suspend fun f2() = coroutineScope {
@@ -79,6 +454,64 @@ suspend fun f2() = coroutineScope {
     deferred1 to deferred2
 }
 
+suspend fun f3() = coroutineScope {
+    val deferred1 : Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException("From async")
+    }
+
+    val deferred2: Deferred<Int> = async {
+        println("Computing deferred2 in async")
+        2
+    }
+
+    try {
+        deferred1.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    deferred1 to deferred2
+}
+
+suspend fun f4() = coroutineScope {
+    val deferred1 : Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException("From async")
+    }
+
+    val deferred2: Deferred<Int> = async {
+        println("Computing deferred2 in async")
+        2
+    }
+
+    try {
+        deferred1.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    deferred2.await()
+}
+
+suspend fun f5() = coroutineScope {
+    val deferred1 : Deferred<Int> = async {
+        println("Throwing exception from async")
+        throw ArithmeticException("From async")
+    }
+
+    try {
+        deferred1.await()
+    } catch (e: Exception) {
+        println("Awaited on deferred")
+        println("Caught inside: $e")
+    }
+
+    2
+}
+
 
 fun main() {
     runBlocking {
@@ -90,17 +523,193 @@ fun main() {
         }
         println()
 
-        println("***** f1(0)")
+        println("***** f1Launch(0)")
         try {
-            println(f1(0))
+            println(f1Launch(0))
         } catch (e: Exception) {
             println("Caught outside: $e")
         }
         println()
 
-        println("***** f1(1000)")
+        println("***** f1LaunchNoJoin(0)")
         try {
-            println(f1(1000))
+            println(f1LaunchNoJoin(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchJoinAtEnd(0)")
+        try {
+            println(f1LaunchJoinAtEnd(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1Async(0)")
+        try {
+            println(f1Async(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncNoJoin(0)")
+        try {
+            println(f1AsyncNoJoin(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncJoinAtEnd(0)")
+        try {
+            println(f1AsyncJoinAtEnd(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1Launch(1000)")
+        try {
+            println(f1Launch(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchNoJoin(1000)")
+        try {
+            println(f1LaunchNoJoin(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchJoinAtEnd(1000)")
+        try {
+            println(f1LaunchJoinAtEnd(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1Async(1000)")
+        try {
+            println(f1Async(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncNoJoin(1000)")
+        try {
+            println(f1AsyncNoJoin(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncJoinAtEnd(1000)")
+        try {
+            println(f1AsyncJoinAtEnd(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchS(0)")
+        try {
+            println(f1LaunchS(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchNoJoinS(0)")
+        try {
+            println(f1LaunchNoJoinS(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchJoinAtEndS(0)")
+        try {
+            println(f1LaunchJoinAtEndS(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncS(0)")
+        try {
+            println(f1AsyncS(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncNoJoinS(0)")
+        try {
+            println(f1AsyncNoJoinS(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncJoinAtEndS(0)")
+        try {
+            println(f1AsyncJoinAtEndS(0))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchS(1000)")
+        try {
+            println(f1LaunchS(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchNoJoinS(1000)")
+        try {
+            println(f1LaunchNoJoinS(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1LaunchJoinAtEndS(1000)")
+        try {
+            println(f1LaunchJoinAtEndS(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncS(1000)")
+        try {
+            println(f1AsyncS(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncNoJoinS(1000)")
+        try {
+            println(f1AsyncNoJoinS(1000))
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f1AsyncJoinAtEndS(1000)")
+        try {
+            println(f1AsyncJoinAtEndS(1000))
         } catch (e: Exception) {
             println("Caught outside: $e")
         }
@@ -108,11 +717,31 @@ fun main() {
 
         println("***** f2()")
         try {
-            val (deferred1, deferred2) = f2()
-            // The exception will be thrown just by invoking F2, without having to wait on the
-            // deferreds
-//            println(deferred2.await())
-//            println(deferred1.await())
+            println(f2())
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f3()")
+        try {
+            println(f3())
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f4()")
+        try {
+            println(f4())
+        } catch (e: Exception) {
+            println("Caught outside: $e")
+        }
+        println()
+
+        println("***** f5()")
+        try {
+            println(f5())
         } catch (e: Exception) {
             println("Caught outside: $e")
         }
