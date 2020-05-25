2f477e63178696f54992eef48c40863b569189fe
PVillela <pvillela@gmail.com>
2020-05-25 16:39:38 -0400

    Completed CoroutineCancellationWithSideEffects.

--- src/main/kotlin/tryout/CoroutineCancellationWithSideEffects.kt
+++ src/main/kotlin/tryout/CoroutineCancellationWithSideEffects.kt
@@ -4,14 +4,13 @@ import arch.util.fatal
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.supervisorScope
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.yield
 
@@ -19,12 +18,12 @@ object CoroutineCancellationWithSideEffects {
 
     suspend fun <T> cancellationWithSideEffectsPattern(
             executionScope: CoroutineScope,
-            internal: suspend CoroutineScope.() -> T,
+            block: suspend CoroutineScope.() -> T,
             exceptionSideEffects: suspend CoroutineScope.(Exception) -> Unit,
             finalSideEffects: () -> Unit
     ): Pair<Deferred<T>, Deferred<T>> = with(executionScope) {
-        // Shield executionScope from exceptions in internal.
-        val internalJob: Deferred<T> = async { internal() }
+        // Shield executionScope from exceptions in block.
+        val internalJob: Deferred<T> = async(SupervisorJob()) { block() }
 
         val externalJob = async {
             try {
@@ -33,21 +32,37 @@ object CoroutineCancellationWithSideEffects {
                 if (fatal(e)) throw e
                 withContext<T>(NonCancellable) {
                     exceptionSideEffects(e)
-                    internalJob.cancelAndJoin()
+                    internalJob.cancel("Cancelled externally", e)
+                    internalJob.join()
                     throw e
                 }
             } finally {
                 finalSideEffects()
             }
-            internal()
         }
 
         return@with Pair(internalJob, externalJob)
     }
 
+    suspend fun CoroutineScope.internalNormal(): Int =
+            try {
+                delay(400)
+                42
+            } catch (e: CancellationException) {
+                println("@@@ internalJob cancelled @@@")
+                delay(50)
+                println("internalJob doing stuff during cancellation.")
+                99
+            }
+
+    suspend fun CoroutineScope.internalAbnormal(): Int {
+        delay(50)
+        throw Exception("My internal job exception")
+    }
+
     suspend fun CoroutineScope.exceptionSideEffects(e: Exception) {
         println("%%% exceptionSideEffects: internalJob cancelled due to $e ... %%%")
-        delay(100)
+        delay(300)
         println("%%% ... finished processing exception side-effects. %%%")
     }
 
@@ -55,26 +70,10 @@ object CoroutineCancellationWithSideEffects {
         println("%%% final side-effects %%%")
     }
 
-    suspend fun CoroutineScope.internalNormal(): Int =
-            try {
-                delay(1000)
-                42
-            } catch (e: CancellationException) {
-                println("%%% internalJob cancelled %%%")
-                delay(1000)
-                println("internalJob doing stuff during cancellation.")
-                99
-            }
-
-    suspend fun CoroutineScope.internalAbnormal(): Int {
-        delay(1000)
-        throw Exception("My internal job exception")
-    }
-
     suspend fun CoroutineScope.exampleWithNormalTerminationOfInner(): Pair<Deferred<Int>, Deferred<Int>> =
             tryout.CoroutineCancellationWithSideEffects.cancellationWithSideEffectsPattern(
                     executionScope = this,
-                    internal = { internalNormal() },
+                    block = { internalNormal() },
                     exceptionSideEffects = { exceptionSideEffects(it) },
                     finalSideEffects = { finalSideEffects() }
             )
@@ -82,7 +81,7 @@ object CoroutineCancellationWithSideEffects {
     suspend fun CoroutineScope.exampleWithAbnormalTerminationOfInner(): Pair<Deferred<Int>, Deferred<Int>> =
             tryout.CoroutineCancellationWithSideEffects.cancellationWithSideEffectsPattern(
                     executionScope = this,
-                    internal = { internalAbnormal() },
+                    block = { internalAbnormal() },
                     exceptionSideEffects = { exceptionSideEffects(it) },
                     finalSideEffects = { finalSideEffects() }
             )
@@ -98,7 +97,7 @@ object CoroutineCancellationWithSideEffects {
     @JvmStatic
     fun main(args: Array<String>) {
 
-        runBlocking {
+        runBlocking(Dispatchers.Default) {
             println("\n********************")
             println("""
                     When internalJob terminates normally, awaiting on externalJob produces the same
@@ -111,7 +110,25 @@ object CoroutineCancellationWithSideEffects {
             safeAwait("internalJob:", internalJob)
         }
 
-        runBlocking {
+        try {
+            runBlocking(Dispatchers.Default) {
+                println("\n********************")
+                println("""
+                    When internalJob terminates abnormally, externalJob terminates with 
+                    internalJob's exception and all cancellation side-effects are executed. 
+                    executionScope also terminates with the same exception.
+                    ---
+                    """.trimIndent())
+                val (internalJob, externalJob) = exampleWithAbnormalTerminationOfInner()
+                yield() // Required for the examples using delay with runBlocking.
+                safeAwait("externalJob:", externalJob)
+                safeAwait("internalJob:", internalJob)
+            }
+        } catch (e: Exception) {
+            println("Parent scope terminated with $e, cause=${e.cause}")
+        }
+
+        runBlocking(Dispatchers.Default) {
             println("\n********************")
             println("""
                     When internalJob is cancelled, externalJob will propagate the cancellation 
@@ -120,47 +137,63 @@ object CoroutineCancellationWithSideEffects {
                     """.trimIndent())
             val (internalJob, externalJob) = exampleWithNormalTerminationOfInner()
             yield() // Required for the examples using delay with runBlocking.
-            internalJob.cancel("Cancellation from the inside", ArithmeticException("Cancellation from the inside"))
+            internalJob.cancel("internalJob cancelled", ArithmeticException("internalJob cancelled"))
+            yield()
             safeAwait("externalJob:", externalJob)
             safeAwait("internalJob:", internalJob)
         }
 
-        runBlocking {
-            println("\n********************")
-            println("""
-                    When internalJob terminates abnormally, externalJob terminates with the same
-                    exception and all cancellation side-effects are executed.
-                    ---
-                    """.trimIndent())
-        }
-
-        runBlocking {
+        runBlocking(Dispatchers.Default) {
             println("\n********************")
             println("""
                     When externalJob is cancelled while internalJob is active, internalJob is cancelled
                     and all cancellation side-effects are executed.
                     ---
                     """.trimIndent())
+            val (internalJob, externalJob) = exampleWithNormalTerminationOfInner()
+            yield() // Required for the examples using delay with runBlocking.
+            externalJob.cancel("externalJob cancelled", ArithmeticException("externalJob cancelled"))
+            yield()
+            safeAwait("externalJob:", externalJob)
+            safeAwait("internalJob:", internalJob)
         }
 
-        runBlocking {
+        runBlocking(Dispatchers.Default) {
             println("\n********************")
             println("""
                     When externalJob is cancelled while handling internalJob's termination due to
-                    cancellation, externalJob will propagate the cancellation exception that
-                    terminated internalJob and all cancellation side-effects are executed.
+                    cancellation, externalJob will propagate internalJob's cancellation exception
+                    and all cancellation side-effects are executed.
                     ---
                     """.trimIndent())
+            val (internalJob, externalJob) = exampleWithNormalTerminationOfInner()
+            yield() // Required for the examples using delay with runBlocking.
+            internalJob.cancel("internalJob cancelled", ArithmeticException("internalJob cancelled"))
+            delay(550)
+            externalJob.cancel("externalJob cancelled", ArithmeticException("externalJob cancelled"))
+            safeAwait("externalJob:", externalJob)
+            safeAwait("internalJob:", internalJob)
         }
 
-        runBlocking {
-            println("\n********************")
-            println("""
+        try {
+            runBlocking(Dispatchers.Default) {
+                println("\n********************")
+                println("""
                     When externalJob is cancelled while handling internalJob's termination due to
-                    failure, externalJob will propagate the exception that
-                    terminated internalJob and all cancellation side-effects are executed.
+                    failure, externalJob terminates with a CancellationException
+                    and all cancellation side-effects are executed. 
+                    executionScope terminates with the same exception as internalJob.
                     ---
                     """.trimIndent())
+                val (internalJob, externalJob) = exampleWithAbnormalTerminationOfInner()
+                yield() // Required for the examples using delay with runBlocking.
+                delay(200)
+                externalJob.cancel("externalJob cancelled", ArithmeticException("externalJob cancelled"))
+                safeAwait("externalJob:", externalJob)
+//                safeAwait("internalJob:", internalJob)
+            }
+        } catch (e: Exception) {
+            println("Parent scope terminated with $e, cause=${e.cause}")
         }
     }
 }
