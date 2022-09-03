a01d1e9329bfc7406e5e1b07918221caefcc57f9
PVillela <pvillela@gmail.com>
2022-09-03 15:07:37 -0400

    Enhanced moduleconfig/pushtofunction to demonstrate overriding the app config source and dynamically changing app config properties at runtime.

--- src/main/kotlin/tryout/moduleconfig/pushtofunction/fwk/appCfg.kt
+++ src/main/kotlin/tryout/moduleconfig/pushtofunction/fwk/appCfg.kt
@@ -1,13 +1,19 @@
 package tryout.moduleconfig.pushtofunction.fwk
 
+import java.util.concurrent.atomic.AtomicReference
+
 data class AppCfgInfo (
 	val x: String,
 	val y: Int,
 )
 
+// Additional complexity added here on purpuse to enable dynamic changes to
+// app configuration properties.
+val appCfgInfo = AtomicReference(AppCfgInfo(
+	"xxx",
+	42,
+))
+
 fun getAppConfiguration(): AppCfgInfo {
-	return AppCfgInfo(
-		"xxx",
-		42,
-	)
+	return appCfgInfo.get()
 }
--- src/main/kotlin/tryout/moduleconfig/pushtofunction/run/run.kt
+++ src/main/kotlin/tryout/moduleconfig/pushtofunction/run/run.kt
@@ -6,10 +6,19 @@
 
 package tryout.moduleconfig.pushtofunction.run
 
+import tryout.moduleconfig.pushtofunction.fwk.AppCfgInfo
+import tryout.moduleconfig.pushtofunction.fwk.appCfgInfo
 import tryout.moduleconfig.pushtofunction.startup.fooSfl
 import tryout.moduleconfig.pushtofunction.startup.fooSfl1
 
 fun main() {
 	fooSfl()
 	fooSfl1()
+
+	// Change of app config properties at runtime
+	appCfgInfo.set(AppCfgInfo(
+		"XXX",
+		84
+	))
+	fooSfl()
 }
