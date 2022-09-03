1fbf679b10798d499d05f40925294365c6cf4775
PVillela <pvillela@gmail.com>
2022-09-03 14:47:52 -0400

    Completed implementing moduleconfig.

---
+++ src/main/kotlin/tryout/moduleconfig/pushtofunction/mod/fooSflBoot.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2022 Paulo Villela. All rights reserved.
+ * Use of this source code is governed by the MIT license
+ * that can be found in the LICENSE file.
+ */
+
+package tryout.moduleconfig.pushtofunction.mod
+
+import tryout.moduleconfig.pushtofunction.fwk.AppCfgInfo
+
+fun fooSflCfgAdapter(appCfg: AppCfgInfo): FooSflCfgInfo {
+	return FooSflCfgInfo(
+		appCfg.x,
+	)
+}
+
+fun fooSflBoot(appCfg: () -> AppCfgInfo): FooSflT {
+	val fooSflCfgSrc = FooSflCfgSrc(
+		{ fooSflCfgAdapter(appCfg()) },
+		barBfBoot(appCfg)
+	)
+	return fooSflC(fooSflCfgSrc)
+}
---
+++ src/main/kotlin/tryout/moduleconfig/pushtofunction/run/run.kt
@@ -0,0 +1,15 @@
+/*
+ *  Copyright © 2021 Paulo Villela. All rights reserved.
+ *  Use of this source code is governed by the Apache 2.0 license
+ *  that can be found in the LICENSE file.
+ */
+
+package tryout.moduleconfig.pushtofunction.run
+
+import tryout.moduleconfig.pushtofunction.startup.fooSfl
+import tryout.moduleconfig.pushtofunction.startup.fooSfl1
+
+fun main() {
+	fooSfl()
+	fooSfl1()
+}
